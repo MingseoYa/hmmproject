0e7522ae5fcf96265396965653890878023bec6f
Ms Kim <myungseokim9912@gmail.com>
2022-09-25 17:15:01 +0900

    myungseo again

--- http.js
+++ http.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 //app.js랑 합침
 const path = require("path");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config({path : './.env'});
 
